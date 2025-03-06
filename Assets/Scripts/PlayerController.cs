b44c4ad33a25beabe0364cf1dc8c69f95ef473e9
Karkavin Elisey <karkavin05@gmail.com>
2025-03-06 16:04:42 +0300

    с пары

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using TMPro;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private int lives;
