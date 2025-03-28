2e4bc67764673294627d0a94eb84d09b13e70a2d
AmbeR1n <82877958+AmbeR1n@users.noreply.github.com>
2025-03-28 21:45:54 +0700

    Add music management system with tracks and audio mixer setup

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    private void Start()
+    {
+        // Play the main menu music
+        MusicManager.Instance.PlayMusic("MainMenu");
+    }
     public void StartBtn()
     {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("Game");
+        MusicManager.Instance.PlayMusic("Game");
     }
 }
